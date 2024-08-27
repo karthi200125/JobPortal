ac253fcb7179cf36691eb4b172cb01a26336fd5f
karthi200125 <skarthikeyan25052001@gmail.com>
2024-08-27 11:36:18 +0530

    p

---
+++ app/(pages)/setting/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+const Setting = () => {
+    return (
+        <div className="w-full h-screen py-5 border">
+            
+{/* DATABASE_URL="postgresql://jobDb_owner:vbDFMW2mq0cf@ep-blue-field-a16z5k16.ap-southeast-1.aws.neon.tech/jobDb?sslmode=require"
+# CUNTRY_STATE_CITY_KEY=NHhvOEcyWk50N2Vna3VFTE00bFp3MjFKR0ZEOUhkZlg4RTk1MlJlaA==
+CUNTRY_STATE_CITY_KEY=TlU0bnNRUlRQT0E5VHQ3dDRQakFrNzQzRGFpSmthZ3lvWU03cklEYw==
+CUNTRY_STATE_CITY_BASEURL=https://api.countrystatecity.in/v1 */}
+        </div>
+    )
+}
+
+export default Setting
