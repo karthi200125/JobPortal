32fe24cae4eadef99c5a193877df80f137e03294
karthi200125 <skarthikeyan25052001@gmail.com>
2024-12-12 19:07:19 +0530

    remove languages array

--- data.js
+++ data.js
@@ -22,6 +22,21 @@ export const filterOptions = [
     },
 ];
 
+// languages
+// app/utils/languages.ts
+export const languages = [
+    { label: "English", value: "en" },
+    { label: "French", value: "fr" },
+    { label: "German", value: "de" },
+    { label: "Spanish", value: "es" },
+    { label: "Portuguese", value: "pt" },
+    { label: "Russian", value: "ru" },
+    { label: "Japanese", value: "ja" },
+    { label: "Korean", value: "ko" },
+    { label: "Chinese", value: "zh" },
+];
+
+
 // Landing page navlinks
 export const LpNavJobs = [
     {
