fe0ba59075884209878b8b0465d6bd24cf58303a
karthi200125 <skarthikeyan25052001@gmail.com>
2024-12-12 19:40:56 +0530

    just remove profile views error

--- actions/user/profileViews.ts
+++ actions/user/profileViews.ts
@@ -2,17 +2,17 @@ import { db } from "@/lib/db"
 
 export const ProfileViews = async (currentUserId?: any, userId?: any) => {
     try {
-        await db.user.update({
-            where: {
-                id: userId, 
-            },
-            data: {
-                profileViews: {
-                    push: currentUserId, 
-                },
-            },
-        });
-        return;
+        // await db.user.update({
+        //     where: {
+        //         id: userId, 
+        //     },
+        //     data: {
+        //         profileViews: {
+        //             push: currentUserId, 
+        //         },
+        //     },
+        // });
+        // return;
     } catch (error) {
         console.log(error);
     }
