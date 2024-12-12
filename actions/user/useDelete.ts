867a332634b73b462294e98d850e8f753669a628
karthi200125 <skarthikeyan25052001@gmail.com>
2024-12-12 19:46:07 +0530

    errorsilve in user delete

--- actions/user/useDelete.ts
+++ actions/user/useDelete.ts
@@ -5,28 +5,28 @@ import { db } from "@/lib/db"
 export const UserDelete = async (userId?: any) => {
     try {
         // delete user
-        await db.user.delete({
-            where: {
-                userId
-            }
-        })
+        // await db.user.delete({
+        //     where: {
+        //         userId
+        //     }
+        // })
         // remove rse id from follower and followwr in all users if hwree my id is there
-        await db.user.delete({
-            where: {
-                followers: {
-                    in: userId
-                },
-                followings: {
-                    in: userId
-                },
-                profileViews: {
-                    in: userId
-                },
-                savedJobs: {
-                    in: userId
-                }
-            }
-        })
+        // await db.user.delete({
+        //     where: {
+        //         followers: {
+        //             in: userId
+        //         },
+        //         followings: {
+        //             in: userId
+        //         },
+        //         profileViews: {
+        //             in: userId
+        //         },
+        //         savedJobs: {
+        //             in: userId
+        //         }
+        //     }
+        // })
         return { success: "User Deleted Successfully" }
     } catch (error) {
         return { error: "User Delete failed" }
