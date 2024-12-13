727802216a61da7a7a73999c7b84049a1d0752cf
karthi200125 <skarthikeyan25052001@gmail.com>
2024-12-13 18:13:22 +0530

    solve jobdesc

--- app/Forms/JobDesc.tsx
+++ app/Forms/JobDesc.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 import { useMemo, useRef, useState } from 'react';
 
 // Dynamically import JoditEditor to prevent server-side rendering issues
-const JoditEditor = dynamic(() => import("jodit-react").then(mod => mod.JoditEditor), { ssr: false });
+// const JoditEditor = dynamic(() => import("jodit-react").then(mod => mod.JoditEditor), { ssr: false });
 
 // Define JobDescProps to specify the onJobDesc prop type
 interface JobDescProps {
@@ -34,13 +34,13 @@ const JobDesc: React.FC<JobDescProps> = ({ onJobDesc }) => {
     return (
         <div className="w-full max-h-max flex items-center flex-col">
             <div className="h-full w-full">
-                <JoditEditor
+                {/* <JoditEditor
                     ref={editor}
                     value={content}
                     config={config}
                     onChange={handleChange}
                     className="w-full min-h-[200px] mt-10 bg-white"
-                />
+                /> */}
                 <style>
                     {`.jodit-wysiwyg { height: 300px !important; }`}
                 </style>
