106bacc10d17a262a168f8877725df4afadc1880
karthi200125 <skarthikeyan25052001@gmail.com>
2024-12-16 18:55:45 +0530

    add follow fuvtion to more profiles

--- app/(pages)/userProfile/AboutMe.tsx
+++ app/(pages)/userProfile/AboutMe.tsx
@@ -17,8 +17,8 @@ const AboutMe = ({ profileUser, isLoading, company, isOrg }: AboutMeProps) => {
                 <UserAboutMeSkeleton />
                 :
                 <>
-                    {/* <h5>Email : <b>{profileUser?.email}</b></h5>
-                    <h5>Phone No : <b>{profileUser?.phoneNo}</b></h5> */}
+                    <h5>Email : <b>{profileUser?.email}</b></h5>
+                    <h5>Phone No : <b>{profileUser?.phoneNo}</b></h5>
                     <p className='text-sm text-[var(--lighttext)] mt-5'>
                         {isOrg ? company?.companyAbout : profileUser?.userAbout}
                     </p>
--- app/(pages)/userProfile/MoreProfiles.tsx
+++ app/(pages)/userProfile/MoreProfiles.tsx
@@ -3,15 +3,18 @@
 import { moreProfileUsers } from '@/actions/moreProfileUsers'
 import Batch from '@/components/Batch'
 import Button from '@/components/Button'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
-import React, { memo } from 'react'
+import React, { memo, useTransition } from 'react'
 import { GoPlus } from 'react-icons/go'
 import { IoMdSend } from 'react-icons/io'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import noAvatar from '../../../public/noProfile.webp'
 import MoreProfileSkeleton from '@/Skeletons/MoreProfileSkeleton'
+import Link from 'next/link'
+import { UserFollowAction } from '@/actions/user/UserFollowAction'
+import { userFollow } from '@/app/Redux/AuthSlice'
 
 interface profileUserProps {
     profileUser?: any
@@ -20,15 +23,37 @@ interface profileUserProps {
 
 const MoreProfiles = ({ profileUser, userId }: profileUserProps) => {
 
-    const user = useSelector((state: any) => state.user.user)
+    const user = useSelector((state: any) => state.user.user)    
+    const dispatch = useDispatch()
 
     const currentUser = user?.id === userId
     const id = currentUser ? user?.id : userId
+    const [isPending, startTransition] = useTransition()
+    const queryClient = useQueryClient();
 
     const { data = [], isLoading } = useQuery({
         queryKey: ['getMoreProfiles', id, currentUser, user?.id],
         queryFn: async () => moreProfileUsers(id, currentUser, user?.id),
     });
+
+    const isFollowings = user?.followings.includes(profileUser?.id)
+
+    const HandleFollow = () => {
+        startTransition(() => {
+            const currentUserId = user?.id
+            const userId = profileUser?.id
+            UserFollowAction(currentUserId, userId)
+                .then((data: any) => {
+                    if (data?.success) {
+                        dispatch(userFollow(userId))
+                        queryClient.invalidateQueries({ queryKey: ['getuser', profileUser?.id] })
+                    }
+                    if (data?.error) {
+                        console.log(data?.error)
+                    }
+                })
+        })
+    }
 
     return (
         <div className='w-full min-h-[200px] overflow-hidden rounded-[20px] border space-y-3 p-5'>
@@ -42,14 +67,17 @@ const MoreProfiles = ({ profileUser, userId }: profileUserProps) => {
                             <Image src={moreuser?.userImage || noAvatar.src} alt='' width={50} height={50} className='bg-neutral-200 rounded-full' />
                             <div className='space-y-2'>
                                 <div className='flex flex-row items-center gap-3'>
-                                    <h4 className='font-bold'>{moreuser?.username}</h4>
+                                    <Link href={`/userProfile/${moreuser?.id}`} className='font-bold cursor-pointer trans'>{moreuser?.username}</Link>
                                     {moreuser?.isPro &&
                                         <Batch type='premium' />
                                     }
                                 </div>
                                 <h5>{moreuser?.profession} </h5>
                                 <div className='flex flex-row items-center gap-3'>
-                                    <Button variant='border' icon={<GoPlus size={20} />} className={'!h-[30px]'}>Follow</Button>
+                                    <Button variant='border' isLoading={isPending} className={`${isFollowings && "!bg-[var(--voilet)] text-white"} !h-[30px]`} onClick={HandleFollow} icon={!isFollowings && <GoPlus size={20} />}>
+                                        {!isFollowings ? "Follow" : "Unfollow"}
+                                    </Button>
+                                    {/* <Button variant='border' icon={<GoPlus size={20} />} className={'!h-[30px]'}>Follow</Button> */}
                                     <Button variant='border' icon={<IoMdSend size={15} />} className={'!h-[30px]'}>Message</Button>
                                 </div>
                             </div>
