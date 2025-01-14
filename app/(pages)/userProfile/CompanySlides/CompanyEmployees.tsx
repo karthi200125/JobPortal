4193c22af6bc76ef2ea720a68c7fa02599bef495
karthi200125 <skarthikeyan25052001@gmail.com>
2025-01-14 17:17:23 +0530

    add get company slide emoployees

--- app/(pages)/userProfile/CompanySlides/CompanyEmployees.tsx
+++ app/(pages)/userProfile/CompanySlides/CompanyEmployees.tsx
@@ -1,36 +1,32 @@
 'use client'
 
-import Batch from "@/components/Batch"
-import Image from "next/image"
-import noAvatar from '../../../../public/noProfile.webp'
-import { useQuery } from "@tanstack/react-query"
 import { getCompaniesEmployees } from "@/actions/getCompanyEmployees"
+import EmployeesSkeleton from "@/Skeletons/EmployeesSkeleton"
+import { useQuery } from "@tanstack/react-query"
+import Employee from "../../dashboard/employees/Employee"
 
 const CompanyEmployees = () => {
 
-  const companyId = 1
   const { data = [], isPending } = useQuery({
-    queryKey: ['getCompanyEmployyes'],
-    queryFn: async () => await getCompaniesEmployees(),
+    queryKey: ['getCompanyEmps'],
+    queryFn: async () => await getCompaniesEmployees([10, 12]),
   });
 
   return (
-    <div className="w-full grid grid-cols-3 gap-5">
-      {data?.map((emp: any) => (
-        <div key={emp?.id} className='flex flex-row items-start gap-5 border p-5 rounded-md'>
-
-          <Image src={emp?.userImage || noAvatar.src} alt='' width={50} height={50} className='bg-neutral-200 rounded-full' />
-          <div className='space-y-2'>
-            <div className='flex flex-row items-center gap-3'>
-              <h4 className='font-bold'>{emp?.username}</h4>
-              {emp?.isPro &&
-                <Batch type='premium' />
-              }
-            </div>
-            <h5>{emp?.profession} </h5>
-          </div>
+    <div>
+      {isPending ? (
+        <EmployeesSkeleton />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-5">
+          {data?.length > 0 ? (
+            data.map((emp: any) => (
+              <Employee user={emp} key={emp.id} isVerify={false} />
+            ))
+          ) : (
+            <h3>No Employees yet!</h3>
+          )}
         </div>
-      ))}
+      )}
     </div>
   )
 }
