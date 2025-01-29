fd661f6409c5407c5f2df4009b897459b14511d8
karthi200125 <skarthikeyan25052001@gmail.com>
2025-01-29 19:48:44 +0530

    error solve

--- actions/job/getLpJobs.ts
+++ actions/job/getLpJobs.ts
@@ -4,11 +4,13 @@ import { db } from "@/lib/db"
 
 export const GetLpJobs = async () => {
     try {
-        const allJobs: any = await db.user.findMany();
-        console.log("Fetched Jobs:", allJobs);
+        const allJobs: any = await db.job.findMany({
+            include: {
+                company: true
+            },            
+        });
         return allJobs;
     } catch (err) {
-        console.error("Error fetching jobs:", err);
         return { error: "Get all jobs failed" };
     }
 };
--- app/(pages)/home/LpJobs.tsx
+++ app/(pages)/home/LpJobs.tsx
@@ -2,16 +2,28 @@
 
 import { GetLpJobs } from "@/actions/job/getLpJobs";
 import LpJobsSkeleton from "@/Skeletons/LpJobsSkeleton";
-import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const LpJobs = () => {
-    const { data: jobs, isPending } = useQuery({
-        queryKey: ['getJobs'],
-        queryFn: async () => await GetLpJobs(),
-    });
 
-    console.log(jobs)
+    const [jobs, setJobs] = useState([])
+    const [isLoading, setisLoading] = useState(false)
+
+    useEffect(() => {
+        const getJobs = async () => {
+            try {
+                setisLoading(true)
+                const job = await GetLpJobs()
+                setJobs(job)
+            } catch (err) {
+                console.log(err)
+            } finally {
+                setisLoading(false)
+            }
+        }
+        getJobs()
+    }, [])
 
     return (
         <div className="w-full min-h-screen p-10 space-y-20">
@@ -25,37 +37,38 @@ const LpJobs = () => {
             </div>
 
             {/* Jobs List */}
-            {isPending ? (
+            {isLoading ? (
                 <LpJobsSkeleton />
             ) : (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
                     {jobs?.map((job: any) => (
-                        <div key={job.id} className="bg-white/[0.05] p-5 rounded-xl overflow-hidden space-y-7">
+                        <div key={job.id} className="bg-white p-5 rounded-xl overflow-hidden space-y-7 cursor-pointer trans text-black">
 
                             {/* Job Image & Title */}
                             <div className="space-y-3">
                                 <Image
-                                    src="https://img.freepik.com/premium-vector/color-green-blue-yellow-shape-diagram-colorful-modern-triangle-logo-icon-sign-file-send-document_981536-460.jpg"
+                                    src={job?.company?.companyImage || ''}
                                     alt="Company Logo"
                                     width={100}
                                     height={100}
                                     className="w-[100px] h-[100px] rounded-lg bg-black object-cover"
                                 />
-                                <h3 className="text-xl font-semibold">{job.title}</h3>
-                                <h5 className="text-xs text-neutral-300">
-                                    {job.location} · ${job.salary}
+                                <h3 className="text-xl font-bold">{job.jobTitle}</h3>
+                                <h5 className="text-xs font-semibold">
+                                    {job.mode} Work · ${job.salary}
                                 </h5>
                             </div>
 
                             {/* Job Description */}
-                            <h4 className="text-white/60 text-sm">
-                                {job.description || "No description available."}
+                            <h4 className="text-neutral-600 text-sm">
+                                {/* {job.description || "No description available."} */}
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit earum sed dolorem eum voluptates porro magnam debitis voluptatem quaerat aut?
                             </h4>
 
                             {/* Job Skills */}
                             <div className="flex flex-wrap items-center gap-2">
                                 {job.skills?.map((skill: string, index: number) => (
-                                    <div key={index} className="rounded-full px-5 py-2 bg-black text-white text-sm">
+                                    <div key={index} className="rounded-full px-5 py-2 bg-black text-white text-sm font-bold">
                                         {skill}
                                     </div>
                                 ))}
